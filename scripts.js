3c5bd3322e2107823cf4466f31e737e8fb019bad
Paul Christianson <pacdeveloping@gmail.com>
2024-05-16 23:15:47 -0400

    Some minor edits and the refresh function seems to be working, just need to fix the style so grid actually appears square

--- scripts.js
+++ scripts.js
@@ -21,19 +21,20 @@ addEventListener("mouseover", (event) => {
 
 const button = document.getElementById('Resize')
 button.addEventListener("click", () => {refresh(prompt('Enter grid width, for example 25 for 25 x 25 grid:'))})
-//issue: grid resize only halves grid size each time
 
 function refresh(SIZE) {
+    
     const divs = document.getElementsByClassName('gridItem');
+    
     while (divs.length > 0) { //remember this! all ideas with incrementing index or for..as did not work
         divs[0].remove();
     }
+
     const parent = document.getElementById('container');
-    console.log(SIZE)
+
     for (i=0;i<SIZE*SIZE;i++){
         const div = document.createElement('div');
         div.setAttribute('style','background-color:blue')
-        //div.textContent = 'Hello';
         div.classList.add('gridItem')
         parent.appendChild(div)
     }
